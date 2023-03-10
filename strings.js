98ca5e10d76769ca024c9d7e7f56c4112ceffd49
EstherMwiyeria <esthermkanoro@gmail.com>
2023-03-10 19:15:03 +0300

    first commit

---
+++ strings.js
@@ -0,0 +1,41 @@
+// Extract the first four characters from the string below;
+// "beautiful"
+let a = "beautiful"
+let b = a.slice(8);
+console.log(a[0]+a[1]+a[2]+a[3]);
+
+// Insert the following string at the tenth index of the below variable:
+// "eat"
+// const food = "I did not have appetite today"
+// let statement = "I did not have appetite today"
+const food = "I did not have appetite today";
+console.log(`${food[10]}eat`);
+// console.log(statement.slice(10,index) + eat + slice(index))
+// Count how many times the following string appears in the string variable:
+// 1. "the"
+// 2."s"
+// const story= "She sells sea shells at the sea shore"
+const story= "She sells sea shells at the sea shore";
+console.log()
+
+// Convert the following strings into the specified format:
+// 1. UpperCase: "CONfidant"
+// 2. LowerCase: "amazing", "beautiFUL"
+// 3. Title case "A busy office"
+let c = "CONfidant"
+let d = c.toUpperCase();
+console.log(c.toUpperCase());
+
+let text = "amazing";
+let text1 = text.toLowerCase();
+console.log(text.toLowerCase());
+
+let statement = "beautiFUL"
+let statement1 = statement.toLowerCase();
+console.log(statement.toLowerCase());
+
+// Using JavaScript, find the following words from the following strings:
+// 1. "market"
+// const string1 = "The lady went to the market with her sister";
+// 2. "season"
+// const string2 = "My favorite season is spring"
